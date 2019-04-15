fecd76aa8004f35406fbb04cb559fd8b023f497b
Samuel Albert <samuel.c.albert@gmail.com>
2019-04-14 20:17:36 -0400

    fix port mapping

---
+++ test/start.spec.ts
@@ -0,0 +1,12 @@
+import { init } from '../src/server';
+
+jest.mock('../src/server');
+
+describe('start', () => {
+
+    it('calls init', async () => {
+        expect( init ).not.toHaveBeenCalled();
+        jest.requireActual('./../src/start');
+        expect( init ).toHaveBeenCalled();
+    });
+});
