69b68c77c853037cb7c1571bf078eddfb73a70bc
Garland <garland220@yahoo.com>
2019-05-06 13:37:14 -0400

    fix test

--- test/env.spec.ts
+++ test/env.spec.ts
@@ -2,7 +2,13 @@ import env from '../src/env';
 
 describe('env', () => {
 	it('includes expected keys', async () => {
-		const keys = ['API_PORT', 'API_HOST'];
+		const keys = [
+			'API_PORT',
+			'API_HOST',
+			'COGNITO_CLIENT_ID',
+			'COGNITO_REGION',
+			'COGNITO_USER_POOL_ID',
+		];
 		Object.keys(env).forEach(k => expect(keys).toContain(k));
 	});
 });
