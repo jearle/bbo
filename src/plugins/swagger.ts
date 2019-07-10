6177bec3d4bd41660fc5e70eee66acb2dc629420
Samuel Albert <samuel.c.albert@gmail.com>
2019-07-10 10:42:30 -0400

    Revert "Merge pull request #7 in CON/cd-product-api from feature/DOPS-1161-bamboo-deploy to develop"
    This reverts commit 52a6f7c6c7ebcfe7e99797b43fd6d39f316d7b28, reversing
    changes made to 766d5ffd4b55dc3a5db81af108d01d540ee1466c.

--- src/plugins/swagger.ts
+++ src/plugins/swagger.ts
@@ -1,6 +1,6 @@
 import * as Hapi from 'hapi';
 const HapiSwagger = require('hapi-swagger');
-const Package = require('package');
+const Package = require('./../../package');
 
 const Swagger: Hapi.ServerRegisterPluginObject<any> = {
 	options: {
