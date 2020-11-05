74416c7a21a553eac5d1ee971339b78b6eded800
Jesse Earle <earle.jesse@gmail.com>
2020-11-05 09:56:15 -0500

    updated tests for transactions

--- packages/server/src/apps/transactions-search/services/transactions/index.ts
+++ packages/server/src/apps/transactions-search/services/transactions/index.ts
@@ -9,29 +9,29 @@ export interface TransactionsService {
 }
 
 export interface TransactionSearchParams {
-  limit: Number;
+  limit: number;
 }
 
 export const cleanTransactionSearchParams = (
   searchParams
 ): TransactionSearchParams => {
   const { limit = 10 } = searchParams;
-  
+
   return { limit: parseInt(limit.toString()) };
 };
 
 export const createTransactionsService = ({ client }: TransactionsOptions) => {
   return {
-    async search(params:TransactionSearchParams) {
+    async search(params: TransactionSearchParams) {
       const result = await client.search({
         index: 'test7_multi_pst',
         from: 0,
         size: <any>params.limit,
         body: {
           query: {
-            match_all: {}
-          }
-        }
+            match_all: {},
+          },
+        },
       });
 
       return result;
--- packages/server/src/apps/transactions-search/transactions-search.test.ts
+++ packages/server/src/apps/transactions-search/transactions-search.test.ts
@@ -1,5 +1,6 @@
+import fetch from 'node-fetch';
 import { createApp } from './';
-import { TransactionsService, createTransactionsService, cleanTransactionSearchParams } from './services/transactions';
+import { createTransactionsService } from './services/transactions';
 import { createElasticsearchClient } from '../../services/elasticsearch';
 import { testHealthcheck } from '../../helpers/unit/healthcheck';
 
@@ -9,31 +10,24 @@ const {
   ELASTICSEARCH_PASSWORD,
 } = process.env;
 
-const transactionsService = createTransactionsService({
-  client: createElasticsearchClient({
-    node: ELASTICSEARCH_NODE,
-    username: ELASTICSEARCH_USERNAME,
-    password: ELASTICSEARCH_PASSWORD,
-  }),
+describe(`transactions app`, () => {
+  let app = null;
+
+  beforeEach(() => {
+    const transactionsService = createTransactionsService({
+      client: createElasticsearchClient({
+        node: ELASTICSEARCH_NODE,
+        username: ELASTICSEARCH_USERNAME,
+        password: ELASTICSEARCH_PASSWORD,
+      }),
+    });
+
+    app = createApp({ transactionsService });
+  });
+
+  test(`/healthcheck`, async () => {
+    await testHealthcheck(app);
+  });
+
+  test(`/transactions no params`, () => {});
 });
-
-test(`healthcheck`, async () => {
-  await testHealthcheck(createApp({ transactionsService }));
-});
-
-test('search transactions', async () => {
-  var params = cleanTransactionSearchParams({});
-  const result = await transactionsService.search(params);
-
-  expect(result.body.hits.hits.length).toBeGreaterThanOrEqual(1);
-
-})
-
-test('search transactions with limit should not return more than set limit', async () => {
-  var params = cleanTransactionSearchParams({limit: 5});
-  const result = await transactionsService.search(params);
-
-  expect(result.body.hits.hits.length).toBeLessThanOrEqual(<any>params.limit);
-
-})
-
