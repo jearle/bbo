3501664228d2b76f556b11db2269785e1322db09
Jesse Earle <earle.jesse@gmail.com>
2020-11-05 16:48:42 -0500

    added elasticsearch docker container

--- packages/server/src/services/redis/redis.test.ts
+++ packages/server/src/services/redis/redis.test.ts
@@ -1,9 +1,18 @@
+import { v4 as uuid } from 'uuid';
 import { createRedisClient } from '.';
 
 const { REDIS_URI } = process.env;
 
-test(`createRedisClient`, () => {
+test(`createRedisClient`, async () => {
   const redisClient = createRedisClient({ uri: REDIS_URI });
+  const id = uuid();
+
+  await redisClient.set(id, `bar`);
+
+  const result = await redisClient.get(id);
+  expect(result).toBe(`bar`);
+
+  await redisClient.del(id);
 
   redisClient.end();
 });
