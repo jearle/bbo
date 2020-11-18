9b4e6d40bc50b83751b3abb9418cc0663c68d420
Peter Ferngren <peter.ferngren@datscha.com>
2020-11-18 17:03:14 +0100

    Added environment for data location

--- packages/server/docker/es/preload.sh
+++ packages/server/docker/es/preload.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Adding data"
 
-curl http://localhost:56789/transactions.ndjson --output transactions.ndjson
+curl $DATA_URL --output transactions.ndjson
 
 curl -s -H "Content-Type: application/x-ndjson" -XPOST http://localhost:9200/test7_multi_pst/_bulk --data-binary @/transactions.ndjson
