75ffe51c6c2b1666f012facf0211166cc4764264
Peter Ferngren <peter.ferngren@datscha.com>
2020-12-18 13:35:38 +0100

    Delay creation of connection to be able to catch connection errors

--- packages/server/src/features/healthcheck/services/rca-web-accounts/index.ts
+++ packages/server/src/features/healthcheck/services/rca-web-accounts/index.ts
@@ -6,17 +6,17 @@ type CreateRCAWebAccountsHealthServiceInput = {
   readonly mssqlProvider: MSSQLProvider;
 };
 
-const rcaWebAccountsHealthService = ({ rcaWebAccountsConnection }) => ({
+const rcaWebAccountsHealthService = ({ createRcaWebAccountsConnection }) => ({
   async health() {
     const health = {
       name: 'RCAWebAccounts',
       status: 0,
       msg: 'ok',
     };
+    let connection = null;
     try {
-      const result = await rcaWebAccountsConnection
-        .request()
-        .query('SELECT 0 AS status');
+      connection = await createRcaWebAccountsConnection();
+      const result = await connection.request().query('SELECT 0 AS status');
 
       if (result.recordset[0].status !== 0) {
         health.status = 1;
@@ -30,11 +30,11 @@ const rcaWebAccountsHealthService = ({ rcaWebAccountsConnection }) => ({
         status: 1,
         msg: `${health.name} threw error: ${error.message}`,
       };
+    } finally {
+      if (connection) {
+        await connection.close();
+      }
     }
-  },
-
-  async close() {
-    await rcaWebAccountsConnection.close();
   },
 });
 
@@ -45,11 +45,13 @@ export type RCAWebAccountsHealthService = ReturnType<
 export const createRCAWebAccountsHealthService = async ({
   mssqlProvider,
 }: CreateRCAWebAccountsHealthServiceInput): Promise<RCAWebAccountsHealthService> => {
-  const rcaWebAccountsConnection = await mssqlProvider.connect({
-    database: DATABASE,
-  });
+  const createRcaWebAccountsConnection = async () => {
+    return await mssqlProvider.connect({
+      database: DATABASE,
+    });
+  };
 
   return rcaWebAccountsHealthService({
-    rcaWebAccountsConnection,
+    createRcaWebAccountsConnection,
   });
 };
