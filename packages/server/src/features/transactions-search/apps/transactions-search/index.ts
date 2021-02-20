386923f3d658ef36e6d8ea983fd323e995371827
Michael Cope <mcope@rcanalytics.com>
2021-02-19 20:53:48 -0500

    api-187 fix searchTrends name

--- packages/server/src/features/transactions-search/apps/transactions-search/index.ts
+++ packages/server/src/features/transactions-search/apps/transactions-search/index.ts
@@ -122,7 +122,7 @@ export const createApp = ({
       index,
       request,
       response,
-    } = await transactionsSearchService.getTrends({
+    } = await transactionsSearchService.searchTrends({
       geographyFilter,
       propertyTypeFilter,
       aggregation,
