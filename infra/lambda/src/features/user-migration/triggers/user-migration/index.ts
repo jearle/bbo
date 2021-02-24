bf6fddc76d3820a0b71dc0d363e64cbf241a77a9
Jesse Earle <earle.jesse@gmail.com>
2021-02-24 18:57:52 -0500

    refactored user-migration

--- infra/lambda/src/features/user-migration/triggers/user-migration/index.ts
+++ infra/lambda/src/features/user-migration/triggers/user-migration/index.ts
@@ -1,34 +1,12 @@
+import {
+  UserMigrationEvent,
+  UserMigrationContext,
+  UserMigrationCallback,
+} from './types';
+
+import { userMigrationAuthentication } from './authentication';
+
 import { FetchDoesAuthenticate } from '../../services/authenticate';
-
-type Request = {
-  readonly password: string;
-};
-
-type UserAttributes = {
-  email: string;
-  email_verified: string;
-};
-
-type Response = {
-  userAttributes: UserAttributes;
-  finalUserStatus?: `CONFIRMED`;
-  messageAction?: `SUPPRESS`;
-};
-
-type Event = {
-  readonly userName: string;
-  readonly triggerSource:
-    | `UserMigration_Authentication`
-    | `UserMigration_ForgotPassword`;
-  readonly request: Request;
-  readonly response: Response;
-};
-
-type Context = {
-  readonly succeed: (event: Event) => void;
-};
-
-type Callback = (error?: string | null) => void;
 
 type CreateUserMigrationTriggerInput = {
   readonly fetchDoesAuthenticate: FetchDoesAuthenticate;
@@ -37,45 +15,17 @@ type CreateUserMigrationTriggerInput = {
 export const createUserMigrationTrigger = ({
   fetchDoesAuthenticate,
 }: CreateUserMigrationTriggerInput) => async (
-  event: Event,
-  context: Context,
-  callback: Callback
+  event: UserMigrationEvent,
+  context: UserMigrationContext,
+  callback: UserMigrationCallback
 ) => {
-  const { userName: username, triggerSource } = event;
-  const { password } = event.request;
+  const { triggerSource } = event;
 
-  if (triggerSource === `UserMigration_Authentication`) {
-    console.log(`UserMigration_Authentication`);
-    const { doesAuthenticate, error } = await fetchDoesAuthenticate({
-      username,
-      password,
+  if (triggerSource !== `UserMigration_Authentication`)
+    return userMigrationAuthentication({
+      fetchDoesAuthenticate,
+      event,
+      context,
+      callback,
     });
-
-    console.log(`Does Authenticate`, doesAuthenticate);
-    if (!doesAuthenticate) {
-      console.log(`does not authenticate error`, error);
-      return callback(error);
-    }
-
-    event.response.userAttributes = {
-      email: username,
-      email_verified: `true`,
-    };
-    event.response.finalUserStatus = `CONFIRMED`;
-    event.response.messageAction = `SUPPRESS`;
-
-    console.log(`succeed`, event);
-
-    context.succeed(event);
-    callback(null);
-    return;
-  }
-
-  // console.log(`cognito!`);
-  // console.log(`username`, username);
-  // console.log(`password`, password);
-  // console.log(`doesAuthenticate`, doesAuthenticate);
-  // console.log(`error`, error);
-
-  return;
 };
