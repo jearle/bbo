b18a8c5cbb8045d2706c5d2ca515ca01c5961255
Jonathan Habshoosh <jhabshoosh@rcanalytics.com>
2021-03-16 16:32:36 +0200

    API-162 remove conflicting definitions from cognito verification_message_template

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -177,8 +177,5 @@ resource "aws_cognito_user_pool" "cognito_user_pool" {
 
   verification_message_template {
     default_email_option = "CONFIRM_WITH_CODE"
-    email_message = "Your verification code is {####}. "
-    email_subject = "Your verification code"
-    sms_message = "Your verification code is {####}. "
   }
 }
