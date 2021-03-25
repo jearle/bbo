c2d7e91ac6be3cd7dd354af02befae88f727548e
Jesse Earle <earle.jesse@gmail.com>
2021-03-24 23:15:49 -0400

    updated property menu builder so it's simpler and includes slugs for data-type to consume

--- packages/server/src/features/lookups/services/data-type/index.ts
+++ packages/server/src/features/lookups/services/data-type/index.ts
@@ -2,16 +2,110 @@ type GetDataTypesInputs = {
   readonly propertyType: string;
 };
 
+const getVolumeOptions = (propertyType: string) => {
+  switch (propertyType) {
+    case `commericial`:
+    case `office`:
+    case `industrial`:
+    case `retail`:
+    case `dev-site`: {
+      return [`Total Square Feet`];
+    }
+
+    case `apartment`: {
+      return [`Total Square Feet`, `Total Units`];
+    }
+
+    case `hotel`:
+    case `seniors-housing-care`: {
+      return [`Total Units`];
+    }
+
+    /* istanbul ignore next */
+    default: {
+      throw new Error(`unknown property type`);
+    }
+  }
+};
+
+const getPricingOptions = (propertyType: string) => {
+  switch (propertyType) {
+    case `commericial`:
+    case `office`:
+    case `industrial`:
+    case `retail`:
+    case `dev-site`: {
+      return [`Price Per Square Foot`];
+    }
+
+    case `apartment`: {
+      return [`Price Per Square Foot`, `Price Per Unit`];
+    }
+
+    case `hotel`:
+    case `seniors-housing-care`: {
+      return [`Price Per Unit`];
+    }
+
+    /* istanbul ignore next */
+    default: {
+      throw new Error(`unknown property type`);
+    }
+  }
+};
+
+type Option = {
+  readonly id: string;
+  readonly label: string;
+  readonly options: Option[];
+};
+
+const toOptions = (strings: string[]): Option[] =>
+  strings.map((id) => {
+    const label = id;
+    const options = [];
+
+    return {
+      id,
+      label,
+      options,
+    };
+  });
+
+const createOptions = (propertyType: string) => {
+  const options = [
+    {
+      label: `Volume`,
+      id: `volume`,
+      options: [
+        {
+          label: `Number of Properties`,
+          id: `number-of-properties`,
+          options: [],
+        },
+        ...toOptions(getVolumeOptions(propertyType)),
+      ],
+    },
+    {
+      label: `Pricing`,
+      id: `pricing`,
+      options: [
+        ...toOptions(getPricingOptions(propertyType)),
+        {
+          label: `Cap Rate`,
+          id: `cap-rate`,
+          options: [],
+        },
+      ],
+    },
+  ];
+
+  return options;
+};
+
 const dataTypeService = () => ({
   getDataTypes: ({ propertyType }: GetDataTypesInputs) => {
-    return [
-      `Price`,
-      `# Properties`,
-      `Total Sqft`,
-      `Total Units`,
-      `PPU`,
-      `CapRate`,
-    ];
+    return createOptions(propertyType);
   },
 });
 export type DataTypeService = ReturnType<typeof dataTypeService>;
