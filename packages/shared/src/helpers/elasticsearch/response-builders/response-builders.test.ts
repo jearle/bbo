d92992258495653a103c66d4af73b798b0bfd01c
Michael Cope <mcope@rcanalytics.com>
2021-03-25 18:02:52 -0400

    API-203 should be from string. add unit test

--- packages/shared/src/helpers/elasticsearch/response-builders/response-builders.test.ts
+++ packages/shared/src/helpers/elasticsearch/response-builders/response-builders.test.ts
@@ -1,4 +1,4 @@
-import { getElasticHits } from './index';
+import { getElasticHits, getTrendsDataFromElasticResponse } from './index';
 describe(`response-builders`, () => {
   const esResponse = {
     body: {
@@ -41,6 +41,7 @@ describe(`response-builders`, () => {
       },
     },
   };
+
   it(`should extract the hits from an elasticsearch response`, () => {
     const hits = getElasticHits(esResponse);
     expect(hits).toEqual([
@@ -54,4 +55,134 @@ describe(`response-builders`, () => {
       },
     ]);
   });
+  describe(`minObservability`, () => {
+    const esBucketResp = {
+      body: {
+        "took": 5,
+        "timed_out": false,
+        "_shards": {
+          "total": 4,
+          "successful": 4,
+          "skipped": 0,
+          "failed": 0
+        },
+        "hits": {
+          "total": {
+            "value": 3904,
+            "relation": "eq"
+          },
+          "max_score": null,
+          "hits": []
+        },
+        "aggregations": {
+          "sumPerQuarter": {
+            "buckets": [
+              {
+                "key": "2001-01-01T00:00:00.000Z-2001-04-01T00:00:00.000Z",
+                "from": 978307200000,
+                "from_as_string": "2001-01-01T00:00:00.000Z",
+                "to": 986083200000,
+                "to_as_string": "2001-04-01T00:00:00.000Z",
+                "doc_count": 18,
+                "filteredSum": {
+                  "doc_count": 9,
+                  "sumResult": {
+                    "value": 0.09524021214909023
+                  }
+                }
+              },
+              {
+                "key": "2001-04-01T00:00:00.000Z-2001-07-01T00:00:00.000Z",
+                "from": 986083200000,
+                "from_as_string": "2001-04-01T00:00:00.000Z",
+                "to": 993945600000,
+                "to_as_string": "2001-07-01T00:00:00.000Z",
+                "doc_count": 20,
+                "filteredSum": {
+                  "doc_count": 7,
+                  "sumResult": {
+                    "value": 0.0885921259011541
+                  }
+                }
+              },
+              {
+                "key": "2001-07-01T00:00:00.000Z-2001-10-01T00:00:00.000Z",
+                "from": 993945600000,
+                "from_as_string": "2001-07-01T00:00:00.000Z",
+                "to": 1001894400000,
+                "to_as_string": "2001-10-01T00:00:00.000Z",
+                "doc_count": 16,
+                "filteredSum": {
+                  "doc_count": 3,
+                  "sumResult": {
+                    "value": 0.09269999961058299
+                  }
+                }
+              },
+              {
+                "key": "2001-10-01T00:00:00.000Z-2002-01-01T00:00:00.000Z",
+                "from": 1001894400000,
+                "from_as_string": "2001-10-01T00:00:00.000Z",
+                "to": 1009843200000,
+                "to_as_string": "2002-01-01T00:00:00.000Z",
+                "doc_count": 10,
+                "filteredSum": {
+                  "doc_count": 1,
+                  "sumResult": {
+                    "value": 0.08397576833764712
+                  }
+                }
+              }
+            ]
+          }
+        }
+      }
+    };
+    it(`returns trends data with nulls for pricing avg with < 2 docs`, () => {
+      const trendsCapRateResp = getTrendsDataFromElasticResponse(esBucketResp, 'CAPRATE');
+      const expected = [
+        {
+          "date": "2001-01-01",
+          "value": 0.09524021214909023
+        },
+        {
+          "date": "2001-04-01",
+          "value": 0.0885921259011541
+        },
+        {
+          "date": "2001-07-01",
+          "value": 0.09269999961058299
+        },
+        {
+          "date": "2001-10-01",
+          "value": null
+        }
+      ];
+      expect(trendsCapRateResp).toEqual(expected);
+    });
+
+    it(`returns trends data for volume metrics with no min doc count`, () => {
+      const trendsCapRateResp = getTrendsDataFromElasticResponse(esBucketResp, 'PRICE');
+      const expected = [
+        {
+          "date": "2001-01-01",
+          "value": 0.09524021214909023
+        },
+        {
+          "date": "2001-04-01",
+          "value": 0.0885921259011541
+        },
+        {
+          "date": "2001-07-01",
+          "value": 0.09269999961058299
+        },
+        {
+          "date": "2001-10-01",
+          "value": 0.08397576833764712
+        }
+      ];
+      expect(trendsCapRateResp).toEqual(expected);
+    });
+  });
+  
 });
