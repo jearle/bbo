136f6495161bc14a4d5b40066f7d7e708941e9c7
Jesse Earle <earle.jesse@gmail.com>
2021-04-02 11:18:01 -0400

    fixed issue with tests failing because the quarter changed

--- packages/shared/src/helpers/elasticsearch/response-builders/response-builders.test.ts
+++ packages/shared/src/helpers/elasticsearch/response-builders/response-builders.test.ts
@@ -1,7 +1,6 @@
 import { esBucketResp, esHitsResponse } from '../stubs/responses';
 import { getElasticHits, getTrendsDataFromElasticResponse } from './index';
 describe(`response-builders`, () => {
-
   it(`should extract the hits from an elasticsearch response`, () => {
     const hits = getElasticHits(esHitsResponse);
     expect(hits).toEqual([
@@ -16,52 +15,56 @@ describe(`response-builders`, () => {
     ]);
   });
   describe(`minObservability`, () => {
-
     it(`returns trends data with nulls for pricing avg with < 2 docs`, () => {
-      const trendsCapRateResp = getTrendsDataFromElasticResponse(esBucketResp, 'CAPRATE');
+      const trendsCapRateResp = getTrendsDataFromElasticResponse(
+        esBucketResp,
+        'CAPRATE'
+      );
       const expected = [
         {
-          "date": "2001-01-01",
-          "value": 0.09524021214909023
+          date: '2001-04-01',
+          value: 0.09524021214909023,
         },
         {
-          "date": "2001-04-01",
-          "value": 0.0885921259011541
+          date: '2001-07-01',
+          value: 0.0885921259011541,
         },
         {
-          "date": "2001-07-01",
-          "value": 0.09269999961058299
+          date: '2001-10-01',
+          value: 0.09269999961058299,
         },
         {
-          "date": "2001-10-01",
-          "value": null
-        }
+          date: '2002-01-01',
+          value: null,
+        },
       ];
       expect(trendsCapRateResp).toEqual(expected);
     });
 
     it(`returns trends data for volume metrics with no min doc count`, () => {
-      const trendsCapRateResp = getTrendsDataFromElasticResponse(esBucketResp, 'PRICE');
+      const trendsCapRateResp = getTrendsDataFromElasticResponse(
+        esBucketResp,
+        'PRICE'
+      );
       const expected = [
         {
-          "date": "2001-01-01",
-          "value": 0.09524021214909023
+          date: '2001-04-01',
+          value: 0.09524021214909023,
         },
         {
-          "date": "2001-04-01",
-          "value": 0.0885921259011541
+          date: '2001-07-01',
+          value: 0.0885921259011541,
         },
         {
-          "date": "2001-07-01",
-          "value": 0.09269999961058299
+          date: '2001-10-01',
+          value: 0.09269999961058299,
         },
         {
-          "date": "2001-10-01",
-          "value": 0.08397576833764712
-        }
+          date: '2002-01-01',
+          value: 0.08397576833764712,
+        },
       ];
       expect(trendsCapRateResp).toEqual(expected);
     });
   });
-  
 });
